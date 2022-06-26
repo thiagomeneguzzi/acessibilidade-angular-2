d2507652fdb78190554cfd56e453724377890286
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-06-26 16:14:44 -0300

    created services and modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'a11y-p2';
+
+  @ViewChild('modal') public modalTemplateRef: TemplateRef<any>
+
+
+  public firstName = 'Thiago';
+  public modalRef: ModalRef;
+
+  constructor(
+    private modalService: ModalService
+  ) {}
+
+  public show(): void {
+    this.modalRef = this.modalService.open({
+      templateRef: this.modalTemplateRef,
+      title: 'User Details'
+    });
+  }
 }
---
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -0,0 +1,39 @@
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from "@angular/core";
+import { BodyInjectorService } from "src/app/shared/services/body-injector.service";
+import { ModalConfig } from "../interfaces/modal-config";
+import { ModalComponent } from "../modal.component";
+
+@Injectable()
+export class ModalService {
+
+  private componentFactory: ComponentFactory<ModalComponent>;
+
+  constructor(
+    componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private bodyInjector: BodyInjectorService
+    ) {
+    this.componentFactory = componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
+
+  public open(config: ModalConfig): ModalRef {
+    const componentRef = this.createComponentRef();
+    componentRef.instance.modalConfig = config
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef)
+  }
+
+  private createComponentRef(): ComponentRef<ModalComponent> {
+    return this.componentFactory.create(this.injector);
+  }
+}
+export class ModalRef {
+
+  constructor(private componentRef: ComponentRef<ModalComponent>) {
+
+  }
+
+  public close(): void {
+    this.componentRef.destroy();
+  }
+}
